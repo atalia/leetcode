ae76f7b8798eede66575230d732f92558f69d20d
赵伟 <Wei@zhaoweideMacBook-Pro.local>
2017-09-21 01:05:19 +0800

    563

---
+++ 563.cpp
@@ -0,0 +1,107 @@
+/*
+563. Binary Tree Tilt
+Given a binary tree, return the tilt of the whole tree.
+
+The tilt of a tree node is defined as the absolute difference between the sum of all left subtree node values and the sum of all right subtree node values. Null node has tilt 0.
+
+The tilt of the whole tree is defined as the sum of all nodes' tilt.
+后续遍历变形
+*/
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <vector>
+#include <queue>
+#include <sstream>
+#include <cctype>
+#include <cmath>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+    int findTilt(TreeNode* root) {
+        preorderTraversal(root);
+        return cnt;
+    }
+    Solution():cnt(0){};
+private:
+    int cnt;
+    int preorderTraversal(TreeNode* root)
+    {
+        if(NULL == root)
+            return 0;
+        int l = preorderTraversal(root->left);
+        int r = preorderTraversal(root->right);
+        cnt += abs(l-r);
+        return l+r+root->val;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+TreeNode* stringToTreeNode(string input) {
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+    
+    string item;
+    stringstream ss;
+    ss.str(input);
+    
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode(stoi(item));
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
+    
+    while (true) {
+        TreeNode* node = nodeQueue.front();
+        nodeQueue.pop();
+        
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+        
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+        
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+        
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        TreeNode* root = stringToTreeNode(line);
+        
+        int ret = Solution().findTilt(root);
+        
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
