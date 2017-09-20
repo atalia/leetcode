30770cf4ce9e0d21d1bf251a12d74edbc662170f
赵伟 <Wei@zhaoweideMacBook-Pro.local>
2017-09-21 00:04:40 +0800

    653

--- 653.cpp
+++ 653.cpp
@@ -1,11 +1,6 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
+ 653. Two Sum IV - Input is a BST
+ Given a Binary Search Tree and a target number, return true if there exist two elements in the BST such that their sum is equal to the given target.
  */
 #include <iostream>
 #include <cstdio>
