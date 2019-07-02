30d10819d809f831d61ac6b48dd130ad11fd6ac8
赵伟 <atalia.zhaowei@gmail.com>
2019-07-02 08:20:12 +0800

    #392

--- Scripts.py
+++ Scripts.py
@@ -1,17 +1,21 @@
 # coding:utf-8
 
+
 class Solution(object):
-    def largestNumber(self, nums):
+    def canJump(self, nums):
         """
         :type nums: List[int]
-        :rtype: str
+        :rtype: bool
         """
-        
+        dis = 0
+        size_nums = len(nums)
+        for i in range(size_nums):
+            dis = max(dis, i + nums[i])
+            if dis == i:
+                return False
+            if dis >= size_nums -1:
+                return True
+        return True
 
-if __name__ == '__main__':
-    nums = [824,938,1399,5607,6973,5703,9609,4398,8247]
-    print Solution().largestNumber(nums)
-
-
-#9609 938 8247 824 69735703560743981399
-#9609 938 824 8247 69735703560743981399
+if __name__ == "__main__":
+    print Solution().canJump([3,2,1,0,4])
