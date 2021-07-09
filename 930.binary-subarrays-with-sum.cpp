1baf02ebca0561c041351cd5243e2d8e6afd2dcc
wei.zhao.phoenixstudio <wei.zhao@puzzleplusgames.com>
2021-07-09 15:38:51 +0800

    #930补全双指针法

--- 930.binary-subarrays-with-sum.cpp
+++ 930.binary-subarrays-with-sum.cpp
@@ -13,15 +13,49 @@ using namespace std;
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int sum = 0;
-        map<int, int> cnt;
+        // int sum = 0;
+        // map<int, int> cnt;
+        // int ret = 0;
+        // for(auto num: nums){
+        //     cnt[sum] += 1;
+        //     sum += num;
+        //     ret += cnt[sum-goal];
+        // }
+        // return ret;
+        int left1 = 0, left2 = left1, right=0;
+        int sum = nums[right];
         int ret = 0;
-        for(auto num: nums){
-            cnt[sum] += 1;
-            sum += num;
-            ret += cnt[sum-goal];
+        while(right < nums.size()){
+            while(right < nums.size() && sum < goal){
+            ++right;
+            sum += nums[right]; 
+            }
+            if(right == nums.size()){
+            return ret;
+            }
+
+            // 先找L1
+            while(sum > goal && left1 < right){
+            sum -= nums[left1];
+            ++left1;
+            }
+
+            // 找L2
+            left2 = left1;
+            while(sum == goal && left2 < right && nums[left2] == 0){
+            ++left2;
+            }
+            if(sum == goal){
+            ret += left2 - left1 + 1;
+            }
+            
+            right += 1;
+            if(right < nums.size()){
+            sum += nums[right];
+            }
+
         }
-        return ret;
+    return ret;
     }
 };
 
